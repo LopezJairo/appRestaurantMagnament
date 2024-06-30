3d50a7db3052b6cda5c7b63f5bedde1d591794d3
LopezJairo <jairolopezcabrera@gmail.com>
2024-06-30 20:42:14 -0300

    se configuro la base de datos en conexionbd.php

---
+++ configuraciones/conexionbd.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost"; // Dirección del servidor MySQL
+$username = "admin"; // Usuario de MySQL
+$password = ""; // Contraseña de MySQL
+$dbname = "restaurantmagnament"; // Nombre de la base de datos
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+echo "Conexión exitosa";
+?>
